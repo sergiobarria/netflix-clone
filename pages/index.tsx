c4b4bc80a4e91a5ad273d5290d143d85d91dcc34
Sergio Barria <sergio.barria9210@gmail.com>
2022-01-25 20:48:34 -0500

    feat(client): add email validation

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,3 +1,7 @@
+import React from 'react';
+
+import { useRouter } from 'next/router';
+
 import Image from 'next/image';
 import Link from 'next/link';
 
@@ -6,6 +10,41 @@ import { IoIosArrowForward } from 'react-icons/io';
 import styles from '../styles/Home.module.scss';
 
 export default function HomePage() {
+  const [email, setEmail] = React.useState<string | null>('');
+  const [userMsg, setUserMsg] = React.useState<string | null>('');
+  const router = useRouter();
+
+  const handleOnChangeEmail = (e: React.FormEvent<HTMLInputElement>) => {
+    const email = e.currentTarget.value;
+
+    // Basic email validation
+    if (
+      !email ||
+      !email.match(
+        /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/
+      )
+    ) {
+      setUserMsg('Please add a valid email');
+    } else {
+      setEmail(email);
+    }
+  };
+
+  const handleLoginWithEmail = (e: React.MouseEvent<HTMLButtonElement>) => {
+    e.preventDefault();
+
+    if (email) {
+      if (email === 'example@example.com') {
+        router.push('/browse');
+      } else {
+        setUserMsg('Something went wrong logging in.');
+      }
+    } else {
+      setEmail('');
+      setUserMsg('Please enter an email address');
+    }
+  };
+
   return (
     <div className={styles.container}>
       {/* Header */}
@@ -41,49 +80,36 @@ export default function HomePage() {
                 membership.
               </h3>
               <div className={styles.formContent}>
-                <div>
-                  <label>
-                    <input
-                      type='email'
-                      name='email'
-                      id='email'
-                      autoComplete='email'
-                      maxLength={50}
-                      minLength={5}
-                      placeholder='Email address'
-                    />
-                  </label>
-                </div>
-                <div className={styles.signupBtn}>
-                  {/* <button>
-                    Get Started <IoIosArrowForward />
-                  </button> */}
-                  <Link href='/browse'>
-                    <a>
+                <div className={styles.formInput}>
+                  <div>
+                    <label>
+                      <input
+                        type='email'
+                        name='email'
+                        id='email'
+                        autoComplete='email'
+                        maxLength={50}
+                        minLength={5}
+                        placeholder='Email address'
+                        onChange={handleOnChangeEmail}
+                      />
+                    </label>
+                  </div>
+                  <div>
+                    <button
+                      onClick={handleLoginWithEmail}
+                      className={styles.signupBtn}
+                    >
                       Get Started <IoIosArrowForward />
-                    </a>
-                  </Link>
+                    </button>
+                  </div>
                 </div>
+                {userMsg && <p className={styles.errorMsg}>*{userMsg}</p>}
               </div>
             </form>
           </div>
         </div>
       </div>
-
-      {/* Hero Background */}
-
-      {/* Background Image */}
-      {/* <div
-        style={{
-          // backgroundColor: '#ff0000',
-          height: '90vh',
-          backgroundImage: 'url(/static/signin-bg.jpg)',
-          opacity: '0.6',
-        }}
-      />
-      <div className={styles.banner}>
-       
-      </div> */}
     </div>
   );
 }
